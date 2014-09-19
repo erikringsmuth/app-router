6207f5f8d4eeda6a813b79128fcf41695f6abacb
0155811 <george.sanchez@thomsonreuters.com>
2014-09-19 15:26:39 -0500

    Added gulp karma

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,12 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var inline = require('gulp-inline');
+var karma = require('gulp-karma');
 
 var files = ['src/*.*', 'tests/spec/*.js'];
+
+//exclude the html files
+var testFiles = ['src/*.js', 'tests/spec/*.js'];
 
 gulp.task('lint', function() {
   gulp.src(files)
@@ -28,18 +32,37 @@ gulp.task('minify', function() {
     .pipe(gulp.dest('.'));
 });
 
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'tests/karma.app-router.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      console.log(err);
+    });
+});
+
+//watch
 gulp.task('watch', function() {
-  gulp.run('lint', 'build', 'minify');
-  gulp.watch(files, function() {
-    gulp.run('lint', 'build', 'minify');
+  gulp.watch(testFiles,['lint', 'build', 'minify','test'])
+  .on('change',function(event){
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
 });
 
+//default
 gulp.task('default', function() {
-  gulp.run('lint', 'build', 'minify');
+  gulp.run('watch','lint', 'build', 'minify','test');
 });
 
 // CI build
 gulp.task('ci', function(){
   gulp.run('lint');
+});
+
+gulp.task('dist', function(){
+  gulp.run('lint','build','minify','test');
 });
